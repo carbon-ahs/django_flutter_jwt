9708a621959f55f4b26f772dad97ad2e42e9604b
Ahsan Habib <87078868+carbon-ahs@users.noreply.github.com>
2024-10-21 17:10:42 +0600

    token blacklist added

---
+++ DEV_NOTE.md
@@ -0,0 +1,13 @@
+# Dev Note
+
+## Backend
+
+### Links
+    - https://youtu.be/xjMP0hspNLE?si=VWmHJ-CruqpDPvF4
+
+
+## Mobile app
+
+### Links
+    - https://youtu.be/xjMP0hspNLE?si=VWmHJ-CruqpDPvF4
+
--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 @api_view(["GET"])
@@ -11,3 +13,19 @@ def getRoutes(request):
     ]
 
     return Response(routes)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token["username"] = user.username
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
